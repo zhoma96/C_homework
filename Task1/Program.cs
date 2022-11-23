b9259529c74041082474c22b7fbe68b6f8dad3ae
zhoma96 <zhchyrmashuulu@gmail.com>
2022-11-23 22:39:08 +0300

    Taks1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if(a > b)
+{
+    max = a;
+    Console.WriteLine($"max = {max}");
+}
+else if(a < b)
+{
+    max = b;
+    Console.WriteLine($"max = {max}");
+}
+else
+{
+    Console.WriteLine("Equal");
+}

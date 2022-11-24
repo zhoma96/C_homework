dee4668201b70e6021a4ca8d1c3c4ce811bb9e17
zhoma96 <zhchyrmashuulu@gmail.com>
2022-11-24 06:58:44 +0300

    Task 2 is done

---
+++ Task1/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if(b > max)
+{
+    max = b;
+    Console.WriteLine($"max = {max}");
+}
+else if(c > max)
+{
+    max = c;
+    Console.WriteLine($"max = {max}");
+}

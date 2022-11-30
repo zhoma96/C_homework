5d6956b9e1c34121f33ea9869c2c7d010a01a7c7
zhoma96 <zhchyrmashuulu@gmail.com>
2022-11-30 20:03:12 +0300

    Task 1 is done

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter number between 100 and 999:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(num);
+if(num < 1000 && num > 0)
+{
+    int midNum = (num % 100) / 10;
+    Console.WriteLine(midNum);
+}
+else
+{
+    Console.WriteLine("Error. Please enter only numbers between 100 and 999");
+}

7770b912c2991166ac478185e724098c2e41adf9
zhoma96 <zhchyrmashuulu@gmail.com>
2022-11-30 20:29:18 +0300

    Task 3 is done

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter day number");
+int num = Convert.ToInt32(Console.ReadLine());
+int monday = 1;
+int tuesday = 2;
+int wednesday = 3;
+int thursday = 4;
+int friday = 5;
+int saturday = 6;
+int sunday = 7;
+if(num == saturday || num == sunday)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No, it is not weekend");
+}

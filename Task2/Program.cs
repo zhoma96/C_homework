6fb8504dda35dc08ee425c6b6116a7fe00ea693c
zhoma96 <zhchyrmashuulu@gmail.com>
2022-12-02 22:10:34 +0300

    Task 2 is done

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter number:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(num);
+if(num < 100)
+{
+    Console.WriteLine("No third digit");
+}
+else
+{
+    string numStr = Convert.ToString(num);
+    Console.WriteLine(numStr[2]);
+}
